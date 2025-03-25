874c5386c60cfd85bae510fe2bf4f5b837fc6468
tcampbell01 <teresa.j.campbell@gmail.com>
2025-03-24 21:40:59 -0400

    finishing up

--- app/tutorials/urls.py
+++ app/tutorials/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path
-from tutorials import views as tutorials_views
+from django.contrib import admin
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', tutorials_views.index.as_view(), name='home'),
-    path('api/tutorials/', tutorials_views.tutorial_list),
-    path('api/tutorials/<int:pk>/', tutorials_views.tutorial_detail),
-    path('api/tutorials/published/', tutorials_views.tutorial_list_published)
+    path('admin/', admin.site.urls),
+    path('', include('tutorials.urls')),
+    path('', include('users.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- app/users/urls.py
+++ app/users/urls.py
@@ -3,8 +3,6 @@ from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('register/', user_views.register, name='register'),
@@ -14,4 +12,4 @@ urlpatterns = [
     path('logout/',
          auth_views.LogoutView.as_view(template_name='users/logout.html'),
          name='logout'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
